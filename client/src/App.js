e63818d085751add32df8c82150214e59004e2ce
Alex Wang <alex.yuanda.wang@gmail.com>
2021-02-07 09:58:08 -0500

    setup yelp component, add dotenv

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+function getParams() {
+  let search = window.location.search;
+  return new URLSearchParams(search);
+}
 
 function App() {
+  const [params, setParams] = useState();
+
+  useEffect(() => {
+    // Get query params
+    const params = getParams();
+    setParams({
+      name: params.get('name'),
+      location: params.get('location'),
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>{params?.name}</h1>
     </div>
   );
 }
---
+++ client/src/components/Yelp.jsx
@@ -0,0 +1,1 @@
+function Yelp({ name, location }) {}
