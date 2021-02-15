7ab373806e09832cc167f2ae05180c5a015c2206
Alex Wang <alex.yuanda.wang@gmail.com>
2021-02-14 21:09:22 -0500

    Include both thumbnails and originals from yelp scrape. Display photos using photoswipe

--- client/src/extension_components/Pictures.jsx
+++ client/src/extension_components/Pictures.jsx
@@ -1,12 +1,49 @@
 import { SERVER_URL } from '../constants.js';
 import { useEffect, useState } from 'react';
+import 'photoswipe/dist/photoswipe.css';
+import 'photoswipe/dist/default-skin/default-skin.css';
+import { Gallery, Item } from 'react-photoswipe-gallery';
 import axios from 'axios';
 axios.defaults.baseURL = SERVER_URL;
 
 export function Pictures({ yelpPhotos }) {
+  // Add required metadata to photos for react-photo-gallery
+  function formatPhotosForReactPhotoGallery(photos) {
+    let formattedPhotos = [];
+    if (!photos) return [];
+    for (var i = 0; i < photos.length; i++) {
+      formattedPhotos.push({
+        src: photos[i],
+        width: 1,
+        height: 1,
+      });
+    }
+    return formattedPhotos;
+  }
+  if (!yelpPhotos) {
+    return null;
+  }
   return (
     <div className="foody-pictures-flexbox">
-      <p>Google pictures here</p>
+      <h3>Pictures from Yelp</h3>
+      <Gallery>
+        {yelpPhotos.map((photo) => {
+          var image = new Image();
+          image.src = photo.original;
+          return (
+            <Item
+              original={photo.original}
+              thumbnial={photo.original}
+              width={image.width}
+              height={image.height}
+            >
+              {({ ref, open }) => (
+                <img ref={ref} onClick={open} src={photo.thumbnail} width="150" height="150" />
+              )}
+            </Item>
+          );
+        })}
+      </Gallery>
     </div>
   );
 }
