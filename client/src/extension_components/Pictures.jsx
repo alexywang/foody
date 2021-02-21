071e5a357168a02410e37cfbf1b115d6a2375fc0
Alex Wang <alex.yuanda.wang@gmail.com>
2021-02-20 20:55:57 -0500

    start making my own photo gallery components

--- client/src/extension_components/Pictures.jsx
+++ client/src/extension_components/Pictures.jsx
@@ -4,6 +4,7 @@ import 'photoswipe/dist/photoswipe.css';
 import 'photoswipe/dist/default-skin/default-skin.css';
 import { Gallery, Item } from 'react-photoswipe-gallery';
 import axios from 'axios';
+import './Pictures.css';
 axios.defaults.baseURL = SERVER_URL;
 
 export function Pictures({ yelpPhotos, source }) {
@@ -11,27 +12,58 @@ export function Pictures({ yelpPhotos, source }) {
 
   if (!yelpPhotos) return null;
 
+  function renderWithPhotoswipe() {
+    return (
+      <div className="foody-pictures-flexbox">
+        <h3>Pictures from Yelp</h3>
+        <Gallery>
+          {yelpPhotos.map((photo) => {
+            var image = new Image();
+            image.src = photo.original;
+            return (
+              <Item
+                original={photo.original}
+                thumbnial={photo.original}
+                width={image.width || 600}
+                height={image.height || 600}
+              >
+                {({ ref, open }) => (
+                  <img ref={ref} onClick={open} src={photo.thumbnail} width="125" height="125" />
+                )}
+              </Item>
+            );
+          })}
+        </Gallery>
+      </div>
+    );
+  }
+
+  // return renderWithPhotoswipe();
   return (
-    <div className="foody-pictures-flexbox">
-      <h3>Pictures from Yelp</h3>
-      <Gallery>
-        {yelpPhotos.map((photo) => {
-          var image = new Image();
-          image.src = photo.original;
-          return (
-            <Item
-              original={photo.original}
-              thumbnial={photo.original}
-              width={image.width || 600}
-              height={image.height || 600}
-            >
-              {({ ref, open }) => (
-                <img ref={ref} onClick={open} src={photo.thumbnail} width="125" height="125" />
-              )}
-            </Item>
-          );
-        })}
-      </Gallery>
+    <div className="picutres-container">
+      <h3>Photos from {source}</h3>
+      <FoodyGallery photos={yelpPhotos} />
     </div>
+  );
+}
+
+function FoodyGallery({ photos }) {
+  return (
+    <div className="foody-gallery">
+      {photos.map((photo, id) => {
+        return <FoodyGalleryThumbnail key={id} photo={photo} />;
+      })}
+    </div>
+  );
+}
+
+function FoodyGalleryThumbnail({ photo }) {
+  return (
+    <img
+      className="foody-gallery-thumbnail grow-on-hover"
+      src={photo.thumbnail}
+      width="135"
+      height="135"
+    />
   );
 }
